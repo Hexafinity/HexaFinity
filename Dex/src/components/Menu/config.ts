c3aa646527e53193cb166a9ac98be2eb80a0ae8f
Jones Jin <gsjones@yandex.com>
2022-01-07 12:12:40 +0800

    fork pancake UI

---
+++ Dex/src/components/Menu/config.ts
@@ -0,0 +1,102 @@
+import { MenuEntry } from '@pancakeswap/uikit'
+import { ContextApi } from 'contexts/Localization/types'
+
+const config: (t: ContextApi['t']) => MenuEntry[] = (t) => [
+  {
+    label: t('Home'),
+    icon: 'HomeIcon',
+    href: '/',
+  },
+  {
+    label: t('Exchange'),
+    icon: 'TradeIcon',
+    href: '/swap',
+  },
+  {
+    label: t('Liquidity'),
+    icon: 'TradeIcon',
+    href: '/liquidity',
+  },
+  {
+    label: t('Farms'),
+    icon: 'FarmIcon',
+    href: '/farms',
+  },
+  {
+    label: t('Pools'),
+    icon: 'PoolIcon',
+    href: '/pools',
+  },
+  // {
+  //   label: t('Prediction (BETA)'),
+  //   icon: 'PredictionsIcon',
+  //   href: '/prediction',
+  // },
+  {
+    label: t('Lottery'),
+    icon: 'TicketIcon',
+    href: '/lottery',
+  },
+  // {
+  //   label: t('Collectibles'),
+  //   icon: 'NftIcon',
+  //   href: '/collectibles',
+  // },
+  // {
+  //   label: t('Team Battle'),
+  //   icon: 'TeamBattleIcon',
+  //   href: '/competition',
+  // },
+  // {
+  //   label: t('Teams & Profile'),
+  //   icon: 'GroupsIcon',
+  //   items: [
+  //     {
+  //       label: t('Leaderboard'),
+  //       href: '/teams',
+  //     },
+  //     {
+  //       label: t('Task Center'),
+  //       href: '/profile/tasks',
+  //     },
+  //     {
+  //       label: t('Your Profile'),
+  //       href: '/profile',
+  //     },
+  //   ],
+  // },
+  // {
+  //   label: t('Info'),
+  //   icon: 'InfoIcon',
+  //   href: '/info',
+  // },
+  {
+    label: t('IFO'),
+    icon: 'IfoIcon',
+    href: '/ifo',
+  },
+  {
+    label: t('More'),
+    icon: 'MoreIcon',
+    items: [
+      {
+        label: t('Voting'),
+        href: '/voting',
+      },
+      {
+        label: t('Github'),
+        href: 'https://github.com/pancakeswap',
+      },
+      {
+        label: t('Docs'),
+        href: 'https://docs.pancakeswap.finance',
+      },
+      {
+        label: t('Blog'),
+        href: 'https://pancakeswap.medium.com',
+      },
+    ],
+  },
+]
+
+export default config
