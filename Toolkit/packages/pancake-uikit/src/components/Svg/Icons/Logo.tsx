2de640f1e97e0742dad13b3341bfdcc94795946b
Jones Jin <gsjones@yandex.com>
2022-01-30 11:10:11 +0800

    add ui tool kit

---
+++ Toolkit/packages/pancake-uikit/src/components/Svg/Icons/Logo.tsx
@@ -0,0 +1,180 @@
+import React from "react";
+import Svg from "../Svg";
+import { SvgProps } from "../types";
+
+const Icon: React.FC<SvgProps> = (props) => {
+  return (
+    <Svg viewBox="0 0 1600 1333.3333" height="100" width="120" {...props}>
+      <g transform="matrix(1.3333333,0,0,-1.3333333,0,1333.3333)" id="g10">
+        <g transform="translate(765.0312,214.1582)" id="g12"><path
+          id="path14"
+          fill="#29abe2"
+          d="m 0,0 h -330.062 l -20.299,-35.158 H 20.299 L 0,0.001 Z" /></g><g
+            transform="translate(388.9932,583.71)"
+            id="g16"><path
+            id="path18"
+            fill="#ed1c24"
+            d="m 0,0 h 97.902 l 21.505,37.564 H -21.687 l 21.66,-37.611 z" /></g><g
+              transform="translate(508.3999,621.2744)"
+              id="g20"><path
+            id="path22"
+            fill="#2f4da0"
+            d="m 0,0 -21.504,-37.564 h 1.809 l 59.195,-85.492 17.757,-25.643 14.811,25.643 5.972,10.342 z" /></g><g
+              transform="translate(536.04,496.4385)"
+              id="g24"><path
+            id="path26"
+            fill="#f7931e"
+            d="m 0,0 -48.331,-83.71 h -0.306 l 21.632,-37.564 h 0.361 l 50.465,87.405 z" /></g><g
+              id="g28"><g
+                clip-path="url(#clipPath34)"
+                id="g30"><g
+                  transform="translate(593.6001,498.2187)"
+                  id="g36"><path
+                id="path38"
+                fill="#29abe2"
+                d="M 0,0 -7.16,10.342 -13.132,0 -27.943,-25.644 13.583,-85.617 c 16.236,-23.447 42.94,-37.438 71.459,-37.438 H 85.2 Z" /></g><g
+                  transform="translate(389.0137,412.7285)"
+                  id="g40"><path
+                id="path42"
+                fill="#29abe2"
+                d="M 0,0 -21.504,-37.564 H 120.021 L 98.389,0 Z" /></g><g
+                  transform="translate(388.9658,583.6626)"
+                  id="g44"><path
+                id="path46"
+                fill="#00ff00"
+                d="M 0,0 -21.66,37.612 -92.707,-85.444 h 43.376 z" /></g><g
+                  transform="translate(388.9932,412.7285)"
+                  id="g48"><path
+                id="path50"
+                fill="#8cc63f"
+                d="m 0,0 -49.358,85.49 h -43.376 l 71.047,-123.054 h 0.203 L 0.021,0 Z" /></g><g
+                  transform="translate(438.1704,498.2187)"
+                  id="g52"><path
+                id="path54"
+                fill="#2f4da0"
+                d="M 0,0 -0.108,0.188 -0.216,0 Z" /></g><g
+                  transform="translate(462.937,537.7808)"
+                  id="g56"><path
+                id="path58"
+                fill="#ed1c24"
+                d="m 0,0 h -2.334 l -22.541,-39.374 0.108,-0.188 h 47.036 l 2.323,4.045 z" /></g><g
+                  transform="translate(393.9536,498.2187)"
+                  id="g60"><path
+                id="path62"
+                fill="#2f4da0"
+                d="M 0,0 H 44.001 L 44.109,0.188 22.137,38.342 Z" /></g><g
+                  transform="translate(416.0479,459.9512)"
+                  id="g64"><path
+                id="path66"
+                fill="#8cc63f"
+                d="M 0,0 21.907,38.268 H -22.094 Z" /></g><g
+                  transform="translate(416.0903,536.561)"
+                  id="g68"><path
+                id="path70"
+                fill="#00ff00"
+                d="M 0,0 21.972,-38.154 44.512,1.22 H 0.704 Z" /></g><g
+                  transform="translate(437.9546,498.2187)"
+                  id="g72"><path
+                id="path74"
+                fill="#29abe2"
+                d="m 0,0 -21.907,-38.268 0.747,-1.293 H 22.999 L 0.216,0 Z" /></g><g
+                  transform="translate(460.9531,458.6582)"
+                  id="g76"><path
+                id="path78"
+                fill="#f7931e"
+                d="M 0,0 H 1.523 L 24.252,39.561 H -22.783 Z" /></g><g
+                  transform="translate(811.0068,412.7285)"
+                  id="g80"><path
+                id="path82"
+                fill="#ed1c24"
+                d="m 0,0 h -97.902 l -21.505,-37.564 H 21.687 L 0.027,0.047 Z" /></g><g
+                  transform="translate(691.5996,375.1641)"
+                  id="g84"><path
+                id="path86"
+                fill="#2f4da0"
+                d="m 0,0 21.505,37.564 h -1.81 l -59.195,85.491 -17.757,25.644 -14.81,-25.644 -5.973,-10.341 z" /></g><g
+                  transform="translate(663.96,500)"
+                  id="g88"><path
+                id="path90"
+                fill="#f7931e"
+                d="m 0,0 48.331,83.71 h 0.306 L 27.005,121.274 H 26.644 L -23.821,33.869 Z" /></g><g
+                  transform="translate(606.4004,498.2187)"
+                  id="g92"><path
+                id="path94"
+                fill="#29abe2"
+                d="M 0,0 7.159,-10.341 13.132,0 27.942,25.644 -13.584,85.618 c -16.235,23.447 -42.94,37.438 -71.458,37.438 H -85.2 Z" /></g><g
+                  transform="translate(810.9863,583.71)"
+                  id="g96"><path
+                id="path98"
+                fill="#29abe2"
+                d="M 0,0 21.504,37.564 H -120.021 L -98.39,0 Z" /></g><g
+                  transform="translate(811.0342,412.7754)"
+                  id="g100"><path
+                id="path102"
+                fill="#00ff00"
+                d="M 0,0 21.66,-37.611 92.707,85.443 H 49.331 Z" /></g><g
+                  transform="translate(811.0068,583.71)"
+                  id="g104"><path
+                id="path106"
+                fill="#8cc63f"
+                d="M 0,0 49.358,-85.491 H 92.734 L 21.687,37.564 H 21.483 L -0.021,0 Z" /></g><g
+                  transform="translate(761.8291,498.2187)"
+                  id="g108"><path
+                id="path110"
+                fill="#2f4da0"
+                d="M 0,0 0.108,-0.187 0.216,0 Z" /></g><g
+                  transform="translate(737.0635,458.6572)"
+                  id="g112"><path
+                id="path114"
+                fill="#ed1c24"
+                d="m 0,0 h 2.334 l 22.54,39.374 -0.108,0.188 h -47.035 l -2.324,-4.044 z" /></g><g
+                  transform="translate(806.0459,498.2187)"
+                  id="g116"><path
+                id="path118"
+                fill="#2f4da0"
+                d="m 0,0 h -44.001 l -0.107,-0.187 21.971,-38.155 z" /></g><g
+                  transform="translate(783.9521,536.4868)"
+                  id="g120"><path
+                id="path122"
+                fill="#8cc63f"
+                d="M 0,0 -21.907,-38.268 H 22.094 Z" /></g><g
+                  transform="translate(783.9092,459.877)"
+                  id="g124"><path
+                id="path126"
+                fill="#00ff00"
+                d="M 0,0 -21.972,38.154 -44.512,-1.22 h 43.808 z" /></g><g
+                  transform="translate(762.0449,498.2187)"
+                  id="g128"><path
+                id="path130"
+                fill="#29abe2"
+                d="M 0,0 21.907,38.268 21.16,39.562 H -22.998 L -0.216,0 Z" /></g><g
+                  transform="translate(739.0469,537.7808)"
+                  id="g132"><path
+                id="path134"
+                fill="#f7931e"
+                d="M 0,0 H -1.523 L -24.252,-39.562 H 22.782 Z" /></g><g
+                  transform="translate(434.9692,785.8418)"
+                  id="g136"><path
+                id="path138"
+                fill="#00c400" d="m 0,0 v 0 l -20.299,35.158 -185.329,-321 h 40.597 z" />
+            </g>
+            <g transform="translate(765.0312,214.1592)" id="g140">
+              <path id="path142" fill="#f7931e" d="m 0,0 20.299,-35.159 185.329,321 H 165.03 Z" />
+            </g>
+            <g transform="translate(765.0312,785.8408)" id="g144">
+              <path id="path146" fill="#2f4da0" d="m 0,0 165.03,-285.841 h 40.598 l -185.329,321 z" />
+            </g>
+            <g transform="translate(765.0312,785.8418)" id="g148">
+              <path id="path150" fill="#ed1c24" d="m 0,0 v -0.001 l 20.299,35.159 h -370.66 L -330.062,0 Z" />
+            </g>
+            <g transform="translate(434.9692,214.1582)" id="g152">
+              <path id="path154" fill="#b2ed4c" d="m 0,0 -165.031,285.842 h -40.597 l 185.329,-321 z" />
+            </g>
+          </g>
+        </g>
+      </g>
+    </Svg>
+  );
+};
+
+export default Icon;
